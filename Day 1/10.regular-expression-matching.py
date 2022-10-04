400b765e4827d601f1fdeb91d2075649a0cad4fb
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-04 12:37:33 +0700

    day1-10

---
+++ Day 1/10.regular-expression-matching.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=10 lang=python3
+#
+# [10] Regular Expression Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+        first_match = bool(s) and p[0] in {s[0], '.'}
+        if len(p) >= 2 and p[1] == '*':
+            return (self.isMatch(s, p[2:]) or 
+                    first_match and self.isMatch(s[1:], p))
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
+# @lc code=end
+
