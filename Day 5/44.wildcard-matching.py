44b8faf725efe6f26db767daea47056ddc27b576
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-08 11:34:06 +0700

    day5-4

---
+++ Day 5/44.wildcard-matching.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=44 lang=python3
+#
+# [44] Wildcard Matching
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        i = j = 0   
+        star = -1
+        match = 0
+        while i < len(s):
+            if j < len(p) and (p[j] == '?' or s[i] == p[j]):
+                i += 1
+                j += 1
+            elif j < len(p) and p[j] == '*':
+                star = j
+                match = i
+                j += 1
+            elif star != -1:
+                j = star + 1
+                match += 1
+                i = match
+            else:
+                return False
+        while j < len(p) and p[j] == '*':
+            j += 1
+        return j == len(p)
+# @lc code=end
+
