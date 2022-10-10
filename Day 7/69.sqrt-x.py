f039747391d0443437dbc5256daa0f1489115b36
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-10 10:51:54 +0700

    day7-9

---
+++ Day 7/69.sqrt-x.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        left, right = 1, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+# @lc code=end
+
