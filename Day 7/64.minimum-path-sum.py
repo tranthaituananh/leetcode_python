b22d8f8db8d6b19a5f8029365a9fc1d468ef22ab
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-10 10:51:01 +0700

    day7-4

---
+++ Day 7/64.minimum-path-sum.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=64 lang=python3
+#
+# [64] Minimum Path Sum
+#
+
+# @lc code=start
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                if i == 0:
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        return dp[m - 1][n - 1]
+# @lc code=end
