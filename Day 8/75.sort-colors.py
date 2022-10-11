c2c298a35d635f92e53ab0cab2e3b0cfff24bd84
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-11 11:53:05 +0700

    day8-5

---
+++ Day 8/75.sort-colors.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low, high = 0, len(nums) - 1
+        i = 0
+        while i <= high:
+            if nums[i] == 0:
+                nums[i], nums[low] = nums[low], nums[i]
+                low += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[high] = nums[high], nums[i]
+                high -= 1
+            else:
+                i += 1
+# @lc code=end
