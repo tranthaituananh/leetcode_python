0ac4f1acb30f800aa78f827657597389331de08d
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-11 11:52:14 +0700

    day8-1

---
+++ Day 8/71.simplify-path.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=71 lang=python3
+#
+# [71] Simplify Path
+#
+
+# @lc code=start
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for p in path.split('/'):
+            if p == '..':
+                if stack:
+                    stack.pop()
+            elif p and p != '.':
+                stack.append(p)
+        return '/' + '/'.join(stack)
+# @lc code=end
+
