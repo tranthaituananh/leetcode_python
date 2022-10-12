8a6708b11c3eebdb65a1045e4788d5e88d41e510
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-12 10:41:57 +0700

    day9-6

---
+++ Day 9/86.partition-list.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=86 lang=python3
+#
+# [86] Partition List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        if not head:
+            return None
+        left = ListNode(0)
+        right = ListNode(0)
+        left_head = left
+        right_head = right
+        while head:
+            if head.val < x:
+                left.next = head
+                left = left.next
+            else:
+                right.next = head
+                right = right.next
+            head = head.next
+        right.next = None
+        left.next = right_head.next
+        return left_head.next
+# @lc code=end
+
