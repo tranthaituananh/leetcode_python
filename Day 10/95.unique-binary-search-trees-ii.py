3708ea3fed4ebddb5e0045b77052548e055414e2
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-13 21:04:53 +0700

    day10-5

---
+++ Day 10/95.unique-binary-search-trees-ii.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=95 lang=python3
+#
+# [95] Unique Binary Search Trees II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        if n == 0:
+            return []
+        return self.dfs(1, n)
+
+    def dfs(self, start, end):
+        if start > end:
+            return [None]
+        res = []
+        for i in range(start, end + 1):
+            left = self.dfs(start, i - 1)
+            right = self.dfs(i + 1, end)
+            for l in left:
+                for r in right:
+                    root = TreeNode(i)
+                    root.left = l
+                    root.right = r
+                    res.append(root)
+        return res
+# @lc code=end
+
