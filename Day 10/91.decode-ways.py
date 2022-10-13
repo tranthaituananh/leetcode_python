66a30b3984ef4fa37a5e934c4ab83cf8969c6eb8
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-13 21:00:16 +0700

    day10-1

---
+++ Day 10/91.decode-ways.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=91 lang=python3
+#
+# [91] Decode Ways
+#
+
+# @lc code=start
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == "0":
+            return 0
+        if len(s) == 1:
+            return 1
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s) + 1):
+            if s[i - 1] != "0":
+                dp[i] += dp[i - 1]
+            if s[i - 2] == "1" or (s[i - 2] == "2" and s[i - 1] <= "6"):
+                dp[i] += dp[i - 2]
+        return dp[-1]
+# @lc code=end
+
