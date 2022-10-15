93ca1481d2cdbd80ac10f274546c4510640e28ea
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-15 09:15:17 +0700

    day12-6

---
+++ Day 12/116.populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=116 lang=python3
+#
+# [116] Populating Next Right Pointers in Each Node
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        if root.left:
+            root.left.next = root.right
+        if root.right:
+            root.right.next = root.next.left if root.next else None
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
+# @lc code=end
+
