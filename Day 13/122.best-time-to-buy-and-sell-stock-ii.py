ab0992b72568ff768cb9968e89268df32491b749
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-16 13:40:10 +0700

    day13-2

---
+++ Day 13/122.best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=122 lang=python3
+#
+# [122] Best Time to Buy and Sell Stock II
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        return max_profit
+# @lc code=end
+
