7dc493ad449db0b2e2d423c8633d47a311d01ce0
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-16 13:43:20 +0700

    day13-4

---
+++ Day 13/124.binary-tree-maximum-path-sum.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=124 lang=python3
+#
+# [124] Binary Tree Maximum Path Sum
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.max_sum = float('-inf')
+        self.dfs(root)
+        return self.max_sum
+    
+    def dfs(self, root):
+        if not root:
+            return 0
+        left = max(0, self.dfs(root.left))
+        right = max(0, self.dfs(root.right))
+        self.max_sum = max(self.max_sum, root.val + left + right)
+        return root.val + max(left, right)
+# @lc code=end
+
