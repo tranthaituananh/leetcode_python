5d831b2493959d7439312c27e21149836aa69adb
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-16 13:44:14 +0700

    day13-5

---
+++ Day 13/125.valid-palindrome.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if not s:
+            return True
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+# @lc code=end
+
