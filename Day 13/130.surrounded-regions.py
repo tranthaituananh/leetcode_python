5d02f787f1a0b0ae9a53807c3f547a6381a8991b
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-16 20:38:00 +0700

    day13-10

---
+++ Day 13/130.surrounded-regions.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=130 lang=python3
+#
+# [130] Surrounded Regions
+#
+
+# @lc code=start
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != 'O':
+                return
+            board[i][j] = 'A'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'A':
+                    board[i][j] = 'O'
+                else:
+                    board[i][j] = 'X'
+# @lc code=end
+
