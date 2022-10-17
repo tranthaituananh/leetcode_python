ee4490dc175640bd54ea6b462a0043c4b0146684
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-17 11:43:55 +0700

    day14-4

---
+++ Day 14/134.gas-station.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=134 lang=python3
+#
+# [134] Gas Station
+#
+
+# @lc code=start
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        start = 0
+        tank = 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i + 1
+                tank = 0
+        return start
+# @lc code=end
+
