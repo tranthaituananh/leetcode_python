9aa39f2c6a04db95247b4c075bdf919c3654e5f3
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-17 11:50:29 +0700

    day14-9

---
+++ Day 14/139.word-break.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=139 lang=python3
+#
+# [139] Word Break
+#
+
+# @lc code=start
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[-1]
+# @lc code=end
+
