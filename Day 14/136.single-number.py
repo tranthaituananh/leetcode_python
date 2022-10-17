d9db3d0ffe3ca85122e8ccf6c7a45697d5c8caa5
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-17 11:45:25 +0700

    day14-6

---
+++ Day 14/136.single-number.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=136 lang=python3
+#
+# [136] Single Number
+#
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(lambda x, y: x ^ y, nums)
+# @lc code=end
+
