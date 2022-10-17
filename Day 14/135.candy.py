c0f217ee2cbd33fb6147a7b64f7910c572fe916a
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-17 11:44:30 +0700

    day14-5

---
+++ Day 14/135.candy.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=135 lang=python3
+#
+# [135] Candy
+#
+
+# @lc code=start
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        return sum(candies)
+# @lc code=end
+
