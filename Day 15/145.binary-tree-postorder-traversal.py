b467dbeb74a5616f4d841e997b87a8cf70002c58
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-18 10:35:06 +0700

    day15-5

---
+++ Day 15/145.binary-tree-postorder-traversal.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=145 lang=python3
+#
+# [145] Binary Tree Postorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+# @lc code=end
+
