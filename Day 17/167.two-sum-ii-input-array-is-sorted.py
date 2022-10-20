3f969f4a7b7715a09a4483282958a27b58d96be3
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-20 22:59:27 +0700

    day17-1

---
+++ Day 17/167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input Array Is Sorted
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers, target):
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+# @lc code=end
+
