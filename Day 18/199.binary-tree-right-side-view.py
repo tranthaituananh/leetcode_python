1a95f515cd665489fa3e013f30e3b801968fc20a
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-21 10:41:26 +0700

    day18-5

---
+++ Day 18/199.binary-tree-right-side-view.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=199 lang=python3
+#
+# [199] Binary Tree Right Side View
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+        queue = [root]
+        while queue:
+            res.append(queue[-1].val)
+            queue = [child for node in queue for child in (node.left, node.right) if child]
+        return res
+# @lc code=end
+
