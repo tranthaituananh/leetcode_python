30a3377e27ffe6861da04bb711184e9277b88c50
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-22 11:04:17 +0700

    day19-10

---
+++ Day 19/214.shortest-palindrome.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=214 lang=python3
+#
+# [214] Shortest Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        r = s[::-1]
+        for i in range(len(s) + 1):
+            if s.startswith(r[i:]):
+                return r[:i] + s
+# @lc code=end
+
