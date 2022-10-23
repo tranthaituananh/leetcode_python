0b6aeac0e9f66131c2345cc87f3506877691cfab
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-23 19:39:43 +0700

    day20-5

---
+++ Day 20/219.contains-duplicate-ii.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=219 lang=python3
+#
+# [219] Contains Duplicate II
+#
+
+# @lc code=start
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = {}
+        for i, num in enumerate(nums):
+            if num in seen and i - seen[num] <= k:
+                return True
+            seen[num] = i
+        return False        
+# @lc code=end
+
