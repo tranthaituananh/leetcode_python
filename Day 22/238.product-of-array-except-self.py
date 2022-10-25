ea7c8afd24f44224e1f502a67a3957b522e37a12
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-25 22:45:19 +0700

    day22-4

---
+++ Day 22/238.product-of-array-except-self.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=238 lang=python3
+#
+# [238] Product of Array Except Self
+#
+
+# @lc code=start
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+        for i in range(1, n):
+            res[i] = res[i - 1] * nums[i - 1]
+        right = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+        return res
+# @lc code=end
+
