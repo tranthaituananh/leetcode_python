8e98bc48b889b1943a894aaec37d7ea91b426c98
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-25 22:51:21 +0700

    day22-5

---
+++ Day 22/239.sliding-window-maximum.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=239 lang=python3
+#
+# [239] Sliding Window Maximum
+#
+
+# @lc code=start
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        n = len(nums)
+        res = []
+        q = collections.deque()
+        for i in range(n):
+            while q and nums[q[-1]] < nums[i]:
+                q.pop()
+            q.append(i)
+            if q[0] == i - k:
+                q.popleft()
+            if i >= k - 1:
+                res.append(nums[q[0]])
+        return res
+# @lc code=end
+
