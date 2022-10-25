81e5c100e2bcb74f6c5e6935669403986222bb60
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-25 23:17:53 +0700

    day22-6

---
+++ Day 22/240.search-a-2-d-matrix-ii.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=240 lang=python3
+#
+# [240] Search a 2D Matrix II
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+        return False
+# @lc code=end
+
