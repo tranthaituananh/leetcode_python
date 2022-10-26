109c2495dbbb55b2fa6ca448329754db75c307d3
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-26 13:59:05 +0700

    day23-4

---
+++ Day 23/268.missing-number.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return sum(range(len(nums)+1)) - sum(nums)
+# @lc code=end
+
