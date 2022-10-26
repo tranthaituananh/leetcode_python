f70a8f9b046cc0feb8d45532b31a869e6e45c3f7
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-26 14:00:36 +0700

    day23-6

---
+++ Day 23/274.h-index.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=274 lang=python3
+#
+# [274] H-Index
+#
+
+# @lc code=start
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        for i in range(n):
+            if citations[i] >= n - i:
+                return n - i
+        return 0 
+# @lc code=end
+
