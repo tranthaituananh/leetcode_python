e91bcd94c5ab75f81d16c45a2aae4f890e847148
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-27 22:07:41 +0700

    day24-10

---
+++ Day 24/300.longest-increasing-subsequence.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=300 lang=python3
+#
+# [300] Longest Increasing Subsequence
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            while i != j:
+                m = (i + j) // 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = x
+            size = max(i + 1, size)
+        return size
+# @lc code=end
+
