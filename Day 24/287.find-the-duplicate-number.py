d4e11eb3ae4e9ace89586705f4814e6e32e07197
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-27 22:06:46 +0700

    day24-3

---
+++ Day 24/287.find-the-duplicate-number.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=287 lang=python3
+#
+# [287] Find the Duplicate Number
+#
+
+# @lc code=start
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        if not nums:
+            return
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        fast = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
+# @lc code=end
+
