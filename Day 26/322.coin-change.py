1dc98fdccaf0d3338e9495874a72b50c8d444fb3
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-29 19:31:55 +0700

    day26-5

---
+++ Day 26/322.coin-change.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=322 lang=python3
+#
+# [322] Coin Change
+#
+
+# @lc code=start
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0] + [float('inf')] * amount
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1
+# @lc code=end
+
