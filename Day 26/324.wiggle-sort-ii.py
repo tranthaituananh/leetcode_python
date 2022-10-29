7f77b3ef66fb4de9c8fa4ce0a4d6b302ab3793c8
tranthaituananh <19521219@gm.uit.edu.vn>
2022-10-29 19:38:09 +0700

    day26-6

---
+++ Day 26/324.wiggle-sort-ii.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=324 lang=python3
+#
+# [324] Wiggle Sort II
+#
+
+# @lc code=start
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        half = len(nums[::2])
+        nums[::2], nums[1::2] = nums[:half][::-1], nums[half:][::-1]
+# @lc code=end
+
