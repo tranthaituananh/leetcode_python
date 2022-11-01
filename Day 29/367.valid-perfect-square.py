7c1e3650b0b95bf5c3735c79c38ee403c4720dd5
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-01 21:23:09 +0700

    day29-3

---
+++ Day 29/367.valid-perfect-square.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=367 lang=python3
+#
+# [367] Valid Perfect Square
+#
+
+# @lc code=start
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        left, right = 1, num
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
+# @lc code=end
+
