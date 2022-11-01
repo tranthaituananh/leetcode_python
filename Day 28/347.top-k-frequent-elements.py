35aae69353b2ecf265eb47bb4b975666c543205a
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-01 16:42:30 +0700

    day28-4

---
+++ Day 28/347.top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=347 lang=python3
+#
+# [347] Top K Frequent Elements
+#
+
+# @lc code=start
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [x[0] for x in collections.Counter(nums).most_common(k)]
+# @lc code=end
+
