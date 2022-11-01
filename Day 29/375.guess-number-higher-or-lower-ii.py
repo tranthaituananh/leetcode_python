62d1f82f0ddd00f58f27e517a5f8bd4223b26bd9
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-01 21:30:45 +0700

    day29-9

---
+++ Day 29/375.guess-number-higher-or-lower-ii.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=375 lang=python3
+#
+# [375] Guess Number Higher or Lower II
+#
+
+# @lc code=start
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        for i in range(n, 0, -1):
+            for j in range(i + 1, n + 1):
+                dp[i][j] = min(k + max(dp[i][k - 1], dp[k + 1][j]) for k in range(i, j))
+        return dp[1][n]
+# @lc code=end
+
