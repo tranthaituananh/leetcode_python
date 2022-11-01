3d14a0657a6eb881fed52abdba8c72a757a2ad17
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-01 14:03:02 +0700

    day28-1

---
+++ Day 28/343.integer-break.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=343 lang=python3
+#
+# [343] Integer Break
+#
+
+# @lc code=start
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        for i in range(2, n + 1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], j * (i - j), j * dp[i - j])
+        return dp[n]
+# @lc code=end
+
