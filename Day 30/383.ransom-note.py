8aa2ae8b208005c30efe575cafcc504b7d31f553
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-02 16:43:20 +0700

    day30-6

---
+++ Day 30/383.ransom-note.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=383 lang=python3
+#
+# [383] Ransom Note
+#
+
+# @lc code=start
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        from collections import Counter
+        return not Counter(ransomNote) - Counter(magazine)
+# @lc code=end
+
