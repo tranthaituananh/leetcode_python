10ad2b85ac175322f48955c9a5469451b9195a15
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-17 14:48:50 +0700

    day38-4

---
+++ Day 38/440.k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=440 lang=python3
+#
+# [440] K-th Smallest in Lexicographical Order
+#
+
+# @lc code=start
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def fn(x):
+            ans, diff = 0, 1
+            while x <= n:
+                ans += min(n - x + 1, diff)
+                x *= 10
+                diff *= 10
+            return ans
+        x = 1
+        while k > 1:
+            cnt = fn(x)
+            if k > cnt:
+                k -= cnt
+                x += 1
+            else:
+                k -= 1
+                x *= 10
+        return x
+# @lc code=end
