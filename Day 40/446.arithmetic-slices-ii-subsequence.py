b4e259f0f8698d57976141442e8f05b2e2b5ad92
tranthaituananh <19521219@gm.uit.edu.vn>
2022-11-20 14:16:47 +0700

    day40-1

---
+++ Day 40/446.arithmetic-slices-ii-subsequence.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=446 lang=python3
+#
+# [446] Arithmetic Slices II - Subsequence
+#
+
+# @lc code=start
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [collections.defaultdict(int) for _ in range(n)]
+        res = 0
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] += 1
+                if diff in dp[j]:
+                    dp[i][diff] += dp[j][diff]
+                    res += dp[j][diff]
+        return res
+# @lc code=end
