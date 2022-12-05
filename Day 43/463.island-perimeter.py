b28f69c2cf7139f32b109c19974499bd3bd7c7bc
tranthaituananh <19521219@gm.uit.edu.vn>
2022-12-05 13:16:11 +0700

    day43-3

---
+++ Day 43/463.island-perimeter.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=463 lang=python3
+#
+# [463] Island Perimeter
+#
+
+# @lc code=start
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        perimeter = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
+        return perimeter
+# @lc code=end
+
